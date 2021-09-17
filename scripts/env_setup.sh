853535880bf8a08b9a8a87112dec07b8c8f6631f
アマデウス <kurisusnowdeng@users.noreply.github.com>
2021-09-17 09:26:31 +0800

    .

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -6,6 +6,6 @@ cd python-env
 virtualenv ones
 source ones/bin/activate
 pip install --no-input torch==1.4.0 torchvision==0.5.0 --force-reinstall
-pip install --no-input rpyc pytorch-pretrained-bert matplotlib
+pip install --no-input rpyc pytorch-pretrained-bert matplotlib requests
 pip install --no-input scipy==1.3.1 --force-reinstall
 deactivate
